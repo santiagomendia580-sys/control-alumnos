1ab0095e301cdb4502cf9d5f7416eb8fc019ecad
SantiMendia <santiagomendiamendia580@gmail.com>
2026-07-09 08:04:34 -0600

    instalacion de firebase  a mi proyecto

---
+++ js/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.11.0/firebase-SERVICE.js';
+
+// TODO: Replace the following with your app's Firebase configuration
+const firebaseConfig = {
+  //...
+};
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdSIzhG4y7aL6u2p2L137eVEv7v30tVV0",
+  authDomain: "appmovil-b8704.firebaseapp.com",
+  projectId: "appmovil-b8704",
+  storageBucket: "appmovil-b8704.firebasestorage.app",
+  messagingSenderId: "442684974869",
+  appId: "1:442684974869:web:6a6b8a234caea0735bab1c"
+};
+
+const app = initializeApp(firebaseConfig);
--- scrip.js
+++
@@ -1,4 +0,0 @@
-const boton = document.getElementById("btnEntrar");
-boton.addEventListener("click", function(){
-    alert("bien vienvenidos al sistema de control de alumnos");
-});
